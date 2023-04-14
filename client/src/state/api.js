7d38b9ac9bf8c3e71c04cfe9ce7437b35b476a74
SntSwetlana <snt.swetlana@gmail.com>
2023-04-14 11:12:25 +0300

    Admins page done

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -9,7 +9,8 @@ export const api = createApi({
         "Customers", 
         "Transactions", 
         "Geography", 
-        "Sales"],
+        "Sales",
+        "Admins"],
     endpoints: (build) => ({
         getUser: build.query({
             query: (id) => `general/user/${id}`,
@@ -39,6 +40,10 @@ export const api = createApi({
             query: () => "sales/sales",
             providesTags: ["Sales"],
         }),
+        getAdmins: build.query({
+            query: () => "management/admins",
+            providesTags: ["Admins"],
+        }),
     })
 })
 
@@ -48,5 +53,6 @@ export const {
     useGetCustomersQuery,
     useGetTransactionsQuery,
     useGetGeographyQuery,
-    useGetSalesQuery
+    useGetSalesQuery,
+    useGetAdminsQuery
 } = api;
